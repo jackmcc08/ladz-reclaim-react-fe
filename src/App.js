3f41dea863bfde6766b26b969b1a71b5ee7b55bb
jackmcc08 <jackmcc08@gmail.com>
2021-05-11 17:15:49 +0100

    created simple stamp counter and button for incrementing stamps!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numStamps: 0,
+    }
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          <button type="button" onClick="onClick()">Click me</button>
+  handleClick() {
+    const history = this.state.numStamps
+    const current = history + 1
+    this.setState({
+      numStamps: current,
+    })
+  }
 
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>RECLAIM!</h1>
+        </header>
+        <main className="App-body">
+          <AddStamp onClick={() => this.handleClick()}/>
+          <StampCounter numStamps={this.state.numStamps}/>
+        </main>
+      </div>
+    );
+  }
 }
 
+function AddStamp(props) {
+  return (
+    <button className="addStampButton" onClick={props.onClick}>
+      Add Stamp
+    </button>
+  )
+}
 
+function StampCounter(props) {
+  return (
+    <h3>Stamps: {props.numStamps}</h3>
+  )
+}
 
-
+  //
 
 export default App;
