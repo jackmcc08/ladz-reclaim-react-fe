4ca89925c4f739291fc50e54b25d2306f5c1a01c
Yates101 <alecyates94@gmail.com>
2021-05-13 21:34:23 +0100

    claim reward button displays when all stamps stamped

--- src/newApp.js
+++ src/newApp.js
@@ -1,36 +1,58 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Stamp = ({stamped}) => {
+const Stamp = ({ passStampStatus }) => {
   const [isStamped, setIsStamped] = useState(false)
+
+  const onStampButtonClicked = () => {
+    setIsStamped(true);
+    passStampStatus();
+  }
 
   return (
     <div className="stamp">
       {isStamped ? (
         <button className="filled-stamp">Stamped</button>
       ) : (
-        <button className="empty-stamp" onClick={() => setIsStamped(true)}>Unstamped</button>
+        <button className="empty-stamp" onClick={() => onStampButtonClicked()}>Unstamped</button>
       )}
     </div>
   )
 }
 
 const StampCard = () => {
+
+  const [rewardActive, setRewardActive] = useState(false)
+
+  let stampCount = 0;
+
+  const onStampSubmitted = () => {
+    stampCount += 1;
+    if(stampCount === 10) { setRewardActive(true) }
+  }
+
   return (
     <div className="stampcard">
       <div className="top-row">
-        <Stamp key="1"/>
-        <Stamp key="2"/>
-        <Stamp key="3"/>
-        <Stamp key="4"/>
-        <Stamp />
+        <Stamp key="1" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="2" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="3" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="4" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="5" passStampStatus={() => onStampSubmitted()}/>
       </div>
       <div className="bottom-row">
-        <Stamp key="6"/>
-        <Stamp key="7"/>
-        <Stamp key="8"/>
-        <Stamp key="9"/>
-        <Stamp key="10"/>
+        <Stamp key="6" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="7" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="8" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="9" passStampStatus={() => onStampSubmitted()}/>
+        <Stamp key="10" passStampStatus={() => onStampSubmitted()}/>
+      </div>
+      <div className="reward-button">
+        { rewardActive ? (
+          <button className="claim-button">Claim Reward</button>
+        ) : (
+          <p>Keep on recycling for your next reward!</p>
+        )}
       </div>
     </div>
   )
